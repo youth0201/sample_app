055494d7801f375d2d1d6fbd9df5fa902f110ec4
Yusuke Shimizu <youth0201@gmail.com>
2017-02-08 11:56:56 +0000

    Install the devise

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ ruby '2.0.0'
 
 gem 'rails', '4.0.5'
 gem 'bootstrap-sass', '3.3.1'
+gem 'devise', '3.4.1'
 
 group :development do
   gem 'sqlite3', '1.3.9'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,19 @@ module ApplicationHelper
     end
   end
   
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-danger"
+      when :alert
+        "alert-warning"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
+  
 end
